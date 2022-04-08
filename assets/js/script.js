61498f75cc2c0cb241b6b7a80f77a1f0888bca74
sakau <sakau@usc.edu>
2022-04-08 13:53:13 -0700

    initial page setup

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var startEl = document.querySelector("#start");
+var checkScoresEl = document.querySelector("#check-scores");
+
+var quizStart = function() {
+
+}
+
+var checkScores = function() {
+
+}

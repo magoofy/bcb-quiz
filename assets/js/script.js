31fc2177772c7c9383ec32834525d5eaa924d9fa
sakau <sakau@usc.edu>
2022-04-13 16:48:07 -0700

    add questions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,110 +1,85 @@
 var startEl = document.querySelector("#start");
 var checkScoresEl = document.querySelector("#check-scores");
-var quizContainer = document.getElementById("quiz");
+var quizContainer = document.querySelector("#quiz");
 var timerEl = document.getElementById("timer");
+var quizHeading = document.querySelector(".quiz-heading");
+var quizDes = document.querySelector(".quiz-description");
 
-var questions = [
-    {
-    question: "Which of these is the correct way to return an element?",
-    answers: {
+var questions = {
+
+    // get question and answer ex: questions.questionOneAnswers.a
+    
+    questionOne: "Which of these is the correct way to return an element?",
+    questionOneAnswers : {
         a: "query.Selector = ('element')",
         b: "var = querySelector.document('element')",
         c: "querySelector = 'element'",
         d: "var = document.querySelector('element')"
-        },
-    correctAnswer: "d"
     },
-    {
-    question: "How do you declare an object?",
-    answers: {
+    questionTwo: "How do you declare an object?",
+    questionTwoAnswers: {
         a: "var obj = {}",
         b: "var object = {}",
         c: "var obj = []",
         d: "var object = []"
-        },
-    correctAnswer: "a"
     },
-    {
-    question: "Which of these makes a new function?",
-    answers: {
+    questionThree: "Which of these makes a new function?",
+    questionThreeAnswers: {
         a: "function myFunction()",
         b: "function = myFunciton()",
         c: "var function() = myFunction",
         d: "var function = myFunction()"
-        },
-    correctAnswer: "a"
     },
-    {
-    question: "How many times does the for loop, for(i=0; i < var; i++), iterate if var = 5?",
-    answers: {
+    questionFour: "How many times does the for loop, for(i=0; i < var; i++), iterate if var = 5?",
+    questionFourAnswers: {
         a: "4",
         b: "0",
         c: "5",
         d: "returns null"
-        },
-    correctAnswer: "c"
     },
-    {
-    question: "How do you comment in Javascript?",
-    answers: {
+    questionFive: "How do you comment in Javascript?",
+    questionFourAnswers: {
         a: "/*Comment*/",
         b: "//Comment",
         c: "<!--Comment-->",
-        d: "**Comment"
-        },
-    correctAnswer: "b"
+        d: "\\Comment"
     },
-    {
-    question: "Which of these is not a logical operator?",
-    answers: {
+    questionSix: "Which of these is not a logical operator?",
+    questionSixAnswers: {
         a: "!",
         b: "&&",
         c: "===",
         d: "||"
-        },
-    correctAnswer: "c"
     },
-    {
-    quesiton: "Which is the correct way to add an event listener",
-    answers: {
+    quesitonSeven: "Which is the correct way to add an event listener",
+    questionSevenAnswers: {
         a: "event.addEventListener('click', function())",
         b: "event = addEventListener('click', function())",
         c: "on.click = addEventListner(function())",
         d: "addEventListener.onClick(function())"
-        },
-    correctAnswer: "a"
     },
-    {
-    question: "How do you append a child?",
-    answers: {
+    questionEight: "How do you append a child?",
+    questionEightAnswers: {
         a: "appenChild(child).parent",
         b: "append(child).parent",
         c: "parent.childAppend(child)",
         d: "parent.appendChild(child)"
-        },
-    correctAnswer: "d"
     },
-    {
-    question: "How do you add html onto an element?",
-    answers: {
+    questionNine: "How do you add html onto an element?",
+    questionNineAnswers: {
         a: "element.textContent('text')",
         b: "element.content = 'text'",
         c: "addContent.element('text')",
         d: "element.textContent = ('text')"
-        },
-    correctAnswer: "a"
     },
-    {
-    question: "Which of these sets the attribute of, 'name', with the variable, ID?",
-    answers: {
+    questionTen: "Which of these sets the attribute of, 'name', with the variable, ID?",
+    questionTenAnswers: {
         a: "element.setAttribute('name', ID)",
         b: "element.setAttribute = ('name', ID)",
         c: "element.setAttribute = (name, 'ID')",
         d: "element.setAttribute(ID, name)"
-        },
-    correctAnswer: "a"
     }
-]
+}
 
 // START BUTTON
 startEl.addEventListener("click", function(){
@@ -129,15 +104,20 @@ function countdown() {
     }, 1000);
 }
 
-// QUIZ BUILDER
+// QUIZ
 function buildQuiz() {
-    //var questionContainer = document.createElement("div");
-    //actionContainerEl.className = "task-actions";
 
-    var questionOne = document.createElement("div");
-    editButtonEl.textContent = questionOneAnswers.a, question;
-    editButtonEl.className = "";
-    editButtonEl.setAttribute("data-task-id", taskId);
+    // REMOVES THE START PAGE ELEMENTS
+    quizHeading.parentNode.removeChild(quizHeading);
+    quizDes.parentNode.removeChild(quizDes);
+    startEl.parentNode.removeChild(startEl);
 
-    actionContainerEl.appendChild(editButtonEl);
+    // QUESTIONS
+    var questionOne = document.createElement("ul");
+    questionOne.textContent = questions.questionOne;
+    questionOne.className = "";
+    quizContainer.append(questionOne);
+    var keys = Object.keys(questions.questionOneAnswers);
+    console.log(keys);
+    keys.forEach((key,index) => {console.log(`${key}: ${questions.questionOneAnswers[key]}`)})
 }
